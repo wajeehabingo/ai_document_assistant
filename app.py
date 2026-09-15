dd920ef57881b09b9550b37d71ad7631beb47357
Wajeeha Fatima <fatimawajeeha28@gmail.com>
2026-09-15 19:55:55 +0500

    Update app.py

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ Question:
             with st.spinner("Generating answer..."):
                 chat_completion = client.chat.completions.create(
                     messages=[{"role": "user", "content": prompt}],
-                    model="llama-3.3-70b-versatile"
+                    model="openai/gpt-oss-120b"
                 )
                 answer = chat_completion.choices[0].message.content
 
